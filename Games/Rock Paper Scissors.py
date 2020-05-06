06e51462cd7a975498de9206afc53106e148bbea
SalmanMKC <13schishti@gmail.com>
2020-05-06 12:32:03 +0100

    Create Rock Paper Scissors.py

---
+++ Games/Rock Paper Scissors.py
@@ -0,0 +1,31 @@
+# Rock Paper Scissors game, play with with a friend!
+
+let hand = 0
+input.onGesture(Gesture.Shake, function () {
+    hand = Math.randomRange(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
